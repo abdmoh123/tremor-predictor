b16b4ce13660a88577254f2be6e4b4933b06a8df
abdmoh123 <abdhawisa@gmail.com>
2022-02-20 01:16:15 +0000

    Add rate of change as feature and Remove useless testing data

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ def main():
     x1 = []  # horizontal component (time)
     x2 = []  # noisy tremor output
     y = []  # intended movement (for training)
-    X = []
-
-    # testing data
-    x1_test = []  # horizontal component (time)
-    x2_test = []  # noisy tremor output
-    y_test = []  # intended movement to predict
 
     # reads training csv file
     with open("tremor_training_data.csv", "r") as file:
@@ -24,66 +18,44 @@ def main():
             x1.append(float(row[0]))
             y.append(float(row[1]))
             x2.append(float(row[2]))
-            X.append([float(row[0]), float(row[2])])
 
-    # prints data in the console
+    # prints training data in the console
     print("X1:\n", x1)
     print("X2:\n", x2)
     print("Y:\n", y)
-
-    # reads testing csv file
-    with open("tremor_test_data.csv", "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            x1_test.append(float(row[0]))
-            y_test.append(float(row[1]))
-            x2_test.append(float(row[2]))
-
-    # prints data in the console
-    print("X1 test:\n", x1_test)
-    print("X2 test:\n", x2_test)
-    print("Y test:\n", y_test)
 
     # calculates the change in tremor output
     delta_x = find_delta(x1, x2)
     print("Change in X:\n", delta_x)
 
-    # applies feature scaling
+    # applies feature scaling to training data
     x2 = normalise(x2)
     delta_x = normalise(delta_x)
-    # applies feature scaling to test data
-    x2_test = normalise(x2_test)
 
+    # puts the features in 1 array
     X = np.vstack((x2, delta_x)).T
-    print("Features:\n", X)
-
-    # reshapes the input to the correct format (1 feature with many samples)
-    # x2 = np.array(x2).reshape(-1, 1)
-    # x2_test = np.array(x2_test).reshape(-1, 1)
 
     # SVM with rbf kernel
     regression = svm.SVR(kernel="rbf")
     regression.fit(X, y)
     predictions = regression.predict(X)
 
-    fig, axes = plt.subplots(3)
+    # calculates and prints the percentage accuracy of the model
+    accuracy = calculate_accuracy(predictions, y)
+    print("\nAccuracy: " + str(accuracy) + "%")
 
+    # splits the graph into 2 subplots
+    fig, axes = plt.subplots(2)
     # plots training data in graph
     axes[0].plot(x1, x2, label="Training: Noisy tremor")
     axes[0].plot(x1, y, label="Training: Intended movement")
     axes[0].legend()
-
-    # plots testing data in graph
-    axes[1].plot(x1_test, x2_test, label="Testing: Noisy tremor")
-    axes[1].plot(x1_test, y_test, label="Testing: Intended movement")
-    axes[1].legend()
-
     # plots SVM regression model
-    axes[2].scatter(x1, x2, s=5, label="Noisy data with tremor")
-    axes[2].scatter(x1, y, s=5, label="Intended movement without tremor")
-    axes[2].plot(x1, predictions, label="SVM regression model")
-    axes[2].legend()
-
+    axes[1].scatter(x1, x2, s=5, label="Noisy data with tremor")
+    axes[1].scatter(x1, y, s=5, label="Intended movement without tremor")
+    axes[1].plot(x1, predictions, label="SVM regression model")
+    axes[1].legend()
+    # displays the plots
     plt.show()
 
 
@@ -102,7 +74,7 @@ def find_highest(feature_list):
     return max_value
 
 
-# calculates the change in tremor output
+# finds the change in tremor output
 def find_delta(x1, x2):
     delta_x = []
     t = x1[1]  # gets the time increment (delta t)
@@ -113,6 +85,13 @@ def find_delta(x1, x2):
         else:
             delta_x.append(x2[i] / t)
     return delta_x
+
+
+# calculates how accurate the model is
+def calculate_accuracy(predicted, actual_output):
+    diff = np.absolute(np.subtract(predicted, actual_output))
+    average_diff = np.sum(diff) / len(actual_output)
+    return 100 - average_diff  # returns percentage accuracy
 
 
 if __name__ == '__main__':
