Add grip force. Improve scalability of plotting functions. Rename axes labels to correct units (mm instead of V). Make time 250Hz.

real_tremor_SVM.py
@@ -17,20 +17,20 @@ def main():
     data = read_data(file_name, 200, 4000)  # real tremor data (t, x, y, z, grip force)
     filtered_data = filter_data(data)  # filters data to get an estimate of intended movement (label)
 
-    t = data[0]  # horizontal component (time)
+    time = np.array(data[0], dtype='f') / 250  # samples are measured at a rate of 250Hz
     [x_motion, x_motion_mean, x_motion_sigma] = fh.normalise(data[1], True)  # tremor in x axis (feature 1)
     [x_label, x_label_mean, x_label_sigma] = fh.normalise(filtered_data[1], True)  # intended motion in x axis
     # y_motion = fh.normalise(data[2])  # tremor in y axis
     # y_label = filtered_data[2]  # intended motion in y axis
     # z_motion = fh.normalise(data[3])  # tremor in z axis
     # z_label = filtered_data[3]  # intended motion in z axis
-    # grip_force = data[4]  # grip force pushed on the device by the user
+    grip_force = fh.normalise(data[4])  # grip force pushed on the device by the user
 
     # calculates the rate of change of 3D motion
-    delta_x = fh.normalise(fh.calc_delta(t, x_motion))  # (feature 2)
+    delta_x = fh.normalise(fh.calc_delta(time, x_motion))  # (feature 2)
 
     # finds the optimum value for C (regularisation parameter)
-    # [horizon, C_x] = mf.optimise([x_motion, delta_x], x_label)  # only required to run once (to find optimal values)
+    # [horizon, C_x] = mf.optimise([x_motion, delta_x, grip_force], x_label)  # only required to run once
     C_x = 21.87  # optimal C value = 21.87
     horizon = 5  # optimal horizon value = 5
     print("Regularisation parameter C(x):", C_x, "\nHorizon value:", horizon)
@@ -39,7 +39,7 @@ def main():
     avg_x = fh.normalise(fh.calc_average(x_motion, horizon))  # (feature 3)
 
     # combines the features into 1 array
-    x_features = np.vstack((x_motion, delta_x, avg_x)).T
+    x_features = np.vstack((x_motion, delta_x, avg_x, grip_force)).T
     print("\nX Features:\n", x_features)
 
     # SVM with rbf kernel (x axis)
@@ -64,47 +64,47 @@ def main():
     tremor_accuracy = mf.calc_accuracy(predicted_tremor, actual_tremor)
     print("Tremor accuracy: " + str(tremor_accuracy) + "%")
 
-    # puts all data in a dictionary for passing to the plot function
-    data_dict = {
-        'x_motion': x_motion,
-        'delta_x': delta_x,
-        'avg_x': avg_x,
-        'x_label': x_label,
-        'predictions': predictions
-    }
+    # puts all features in a list for passing to the plot function
+    features = [
+        [x_motion, "X motion"],
+        [avg_x, "Average X motion"],
+        [delta_x, "Delta X motion"],
+        [grip_force, "Grip force"]
+    ]
     # plots data and model (x axis)
-    plot_model(t, data_dict)
-    # puts tremor component data in a dictionary
-    tremor_dict = {
-        'actual_tremor': actual_tremor,
-        'predicted_tremor': predicted_tremor,
-        'tremor_error': tremor_error
-    }
+    plot_model(time, features, x_label, predictions)
+    # puts tremor component data in a list
+    tremors = [
+        [actual_tremor, "Actual tremor"],
+        [predicted_tremor, "Predicted tremor"],
+        [tremor_error, "Tremor error"]
+    ]
     # plots the tremor components (x axis) in a separate graph
-    plot_tremor(t, tremor_dict)
+    plot_tremor(time, tremors)
 
 
 # plots the real tremor data and SVM model (x axis)
-def plot_model(time, data):
+def plot_model(time, features, label, predictions):
     # splits plot window into 2 graphs
     fig, axes = plt.subplots(3)
 
     # plots data
-    axes[0].plot(time, data['x_motion'], label="Noisy data with tremor")
-    axes[0].plot(time, data['x_label'], label="Intended movement without tremor")
+    axes[0].plot(time, features[0][0], label="Noisy data with tremor")
+    axes[0].plot(time, label, label="Intended movement without tremor")
+    axes[0].set(ylabel="X motion (mm)")
     axes[0].legend()
 
     # plots SVM regression model
-    axes[1].plot(time, data['predictions'], label="SVM regression model")
-    axes[1].plot(time, data['x_label'], label="Intended movement without tremor")
-    axes[1].set(ylabel="X motion voltage (V)")
+    axes[1].plot(time, predictions, label="SVM regression model")
+    axes[1].plot(time, label, label="Intended movement without tremor")
+    axes[1].set(ylabel="X motion (mm)")
     axes[1].legend()
 
     # plots the features (normalised)
-    axes[2].plot(time, fh.normalise(data['x_motion']), label="X")
-    axes[2].plot(time, fh.normalise(data['avg_x']), label="Avg X")
-    axes[2].plot(time, fh.normalise(data['delta_x']), label="Delta X")
-    axes[2].set(xlabel="time/index")
+    for feature in features:
+        axes[2].plot(time, fh.normalise(feature[0]), label=feature[1])
+    axes[2].set(ylabel="X motion (normalised)")
+    axes[2].set(xlabel="Index")
     axes[2].legend()
 
     # displays graphs
@@ -112,15 +112,16 @@ def plot_model(time, data):
 
 
 # plots the tremor component of the data and predictions
-def plot_tremor(time, data):
+def plot_tremor(time, tremors):
     # plots tremor data
-    plt.plot(time, data['actual_tremor'], label="Actual tremor", linewidth=0.5)
-    plt.plot(time, data['predicted_tremor'], label="Predicted tremor", linewidth=0.5)
-    plt.plot(time, data['tremor_error'], label="Tremor error", linewidth=0.5)
+    for tremor in tremors:
+        plt.plot(time, tremor[0], label=tremor[1], linewidth=0.5)
+
     # axes labels and legend
-    plt.ylabel("X motion voltage (V)")
-    plt.xlabel("time/index")
+    plt.ylabel("X tremor motion (mm)")
+    plt.xlabel("Index")
     plt.legend()
+
     # displays graphs
     plt.show()
 