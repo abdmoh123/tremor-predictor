Fix predicted tremor component calculation and clip minimum accuracy to 0%

functions/miscellaneous.py
@@ -49,8 +49,11 @@ def find_lowest(features, magnitude=False):
 # calculates the accuracy of the model using root mean squared error
 def calc_accuracy(predicted, actual_output):
     rms_error = mean_squared_error(actual_output, predicted, squared=False)  # calculates the RMS error
-    sigma = np.std(actual_output)  # calculates the standard deviation of voluntary motion
-    return 100 * (1 - (rms_error / sigma))  # uses the standard deviation to convert the RMS error into a percentage
+    nrms_error = rms_error / np.std(actual_output)  # normalises the RMSE using the standard deviation
+    # clips the output to 0 (minimum value is 0%)
+    if nrms_error > 1:
+        return 0
+    return 100 * (1 - nrms_error)  # converts the normalised RMSE to a percentage
 
 
 def calc_tremor_accuracy(input_motion, predictions, voluntary_motion):

real_tremor_SVM.py
@@ -98,13 +98,13 @@ def main():
 
     # gets the tremor component by subtracting from the voluntary motion
     x_actual_tremor = np.subtract(x_motion, x_label)
-    x_predicted_tremor = np.subtract(x_predictions, x_label)
+    x_predicted_tremor = np.subtract(x_motion, x_predictions)
     x_tremor_error = np.subtract(x_actual_tremor, x_predicted_tremor)
     y_actual_tremor = np.subtract(y_motion, y_label)
-    y_predicted_tremor = np.subtract(y_predictions, y_label)
+    y_predicted_tremor = np.subtract(y_motion, y_predictions)
     y_tremor_error = np.subtract(y_actual_tremor, y_predicted_tremor)
     z_actual_tremor = np.subtract(z_motion, z_label)
-    z_predicted_tremor = np.subtract(z_predictions, z_label)
+    z_predicted_tremor = np.subtract(z_motion, z_predictions)
     z_tremor_error = np.subtract(z_actual_tremor, z_predicted_tremor)
     # calculates and prints the normalised RMSE percentage of the tremor component
     x_tremor_accuracy = mf.calc_accuracy(x_predicted_tremor, x_actual_tremor)