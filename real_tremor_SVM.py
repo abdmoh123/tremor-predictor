8a4fb6aa324df690b155754164593caa8efd6036
abdmoh123 <abdhawisa@gmail.com>
2022-04-02 23:33:39 +0100

    Shorten (length of code) multi-dimensional lists by using for loops. Add ability to control ratio of training and testing data. Truncate all printed lists/arrays in the console (threshold = 50).

--- real_tremor_SVM.py
+++ real_tremor_SVM.py
@@ -11,16 +11,19 @@ import functions.miscellaneous as mf
 import functions.evaluator as eva
 import functions.optimiser as op
 
+np.set_printoptions(threshold=50)  # shortens long arrays in the console window
+
 
 def main():
     file_name = "./data/real_tremor_data.csv"
+    training_testing_ratio = 0.8
 
     # reads data into memory and filters it
     data = read_data(file_name, 200, 4000)  # real tremor data (t, x, y, z, grip force)
     filtered_data = filter_data(data)  # filters data to get an estimate of intended movement (label)
     # 80% of data is used for training
-    training_data = data[:, :int(0.8 * len(data[0]))]  # first 80% of data for training
-    filtered_training_data = filtered_data[:, :int(0.8 * len(filtered_data[0]))]  # selects the labels for training
+    training_data = data[:, :int(training_testing_ratio * len(data[0]))]  # first 80% of data for training
+    filtered_training_data = filtered_data[:, :int(training_testing_ratio * len(filtered_data[0]))]  # training labels
     time = np.array(data[0], dtype='f') / 250  # samples are measured at a rate of 250Hz
 
     # training data is assigned
@@ -32,28 +35,22 @@ def main():
 
     # calculates the features in a separate function
     [training_features, horizon, C] = prepare_model(time, training_motion, training_label)
-    # reformats the features for fitting the model
-    features_array = [
-        np.vstack(training_features[0]).T,  # X
-        np.vstack(training_features[1]).T,  # Y
-        np.vstack(training_features[2]).T  # Z
-    ]
-    print("\nTraining features (x):\n", features_array[0])
-    print("Training features (y):\n", features_array[1])
-    print("Training features (z):\n", features_array[2])
 
     # SVM with rbf kernel (x axis)
-    x_regression = svm.SVR(kernel="rbf", C=C[0])
-    y_regression = svm.SVR(kernel="rbf", C=C[1])
-    z_regression = svm.SVR(kernel="rbf", C=C[2])
-    # uses the features the fit the regression model to the data
-    x_regression.fit(features_array[0], training_label[0])
-    y_regression.fit(features_array[1], training_label[1])
-    z_regression.fit(features_array[2], training_label[2])
+    regression = []
+    for i in range(len(training_features)):
+        # reformats the features for fitting the model (numpy array)
+        axis_features = np.vstack(training_features[i]).T
+        regression.append(svm.SVR(kernel="rbf", C=C[i]))
+        # uses the features the fit the regression model to the data
+        regression[i].fit(axis_features, training_label[i])
+    print("\nTraining features (x):\n", np.array(training_features[0]))
+    print("Training features (y):\n", np.array(training_features[1]))
+    print("Training features (z):\n", np.array(training_features[2]))
 
     # 20% of the data is separated and used for testing
-    test_data = data[:, int(0.8 * len(data[0])):]  # last 20% of data for testing
-    filtered_test_data = filtered_data[:, int(0.8 * len(filtered_data[0])):]  # selects the labels for testing
+    test_data = data[:, int(training_testing_ratio * len(data[0])):]  # last 20% of data for testing
+    filtered_test_data = filtered_data[:, int(training_testing_ratio * len(filtered_data[0])):]  # testing labels
     # test data is assigned
     [xt, yt, zt] = select_normalised_data(test_data)  # normalises and assigns the input motion data
     [fxt, fyt, fzt] = select_normalised_data(filtered_test_data)  # normalises and assigns the voluntary motion data
@@ -68,66 +65,44 @@ def main():
 
     # calculates the features in a separate function
     test_features = prepare_model(time, test_motion, test_label, horizon)
-    # reformats the features for fitting the model
-    features_array = [
-        np.vstack(test_features[0]).T,  # X
-        np.vstack(test_features[1]).T,  # Y
-        np.vstack(test_features[2]).T  # Z
-    ]
-    print("\nTest features (x):\n", features_array[0])
-    print("Test features (y):\n", features_array[1])
-    print("Test features (z):\n", features_array[2])
+    # predicts intended motion using the original data as an input (scaled to intended motion)
+    prediction = []
+    for i in range(len(test_features)):
+        axis_features = np.vstack(test_features[i]).T   # reformats the features for fitting the model (numpy array)
+        prediction.append(fh.denormalise(regression[i].predict(axis_features), test_label_mean[i], test_label_sigma[i]))
+    print("\nTest features (x):\n", np.array(test_features[0]))
+    print("Test features (y):\n", np.array(test_features[1]))
+    print("Test features (z):\n", np.array(test_features[2]))
 
     # denormalises the data + labels (to its original scale)
-    motion = [
-        fh.denormalise(test_motion[0], test_motion_mean[0], test_motion_sigma[0]),  # X
-        fh.denormalise(test_motion[1], test_motion_mean[1], test_motion_sigma[1]),  # Y
-        fh.denormalise(test_motion[2], test_motion_mean[2], test_motion_sigma[2])  # Z
-    ]
-    label = [
-        fh.denormalise(test_label[0], test_label_mean[0], test_label_sigma[0]),  # X
-        fh.denormalise(test_label[1], test_label_mean[1], test_label_sigma[1]),  # Y
-        fh.denormalise(test_label[2], test_label_mean[2], test_label_sigma[2])  # Z
-    ]
-
-    # predicts intended motion using the original data as an input (scaled to intended motion)
-    prediction = [
-        fh.denormalise(x_regression.predict(features_array[0]), test_label_mean[0], test_label_sigma[0]),
-        fh.denormalise(y_regression.predict(features_array[1]), test_label_mean[1], test_label_sigma[1]),
-        fh.denormalise(z_regression.predict(features_array[2]), test_label_mean[2], test_label_sigma[2])
-    ]
-    print("\nPredicted output (x):\n", prediction[0], "\nActual output (x):\n", label[0])
-    print("\nPredicted output (y):\n", prediction[1], "\nActual output (y):\n", label[1])
-    print("\nPredicted output (z):\n", prediction[2], "\nActual output (z):\n", label[2])
+    motion = []
+    label = []
+    for i in range(len(test_motion)):
+        motion.append(fh.denormalise(test_motion[i], test_motion_mean[i], test_motion_sigma[i]))
+        label.append(fh.denormalise(test_label[i], test_label_mean[i], test_label_sigma[i]))
+    print("\nPredicted output (x):\n", np.array(prediction[0]), "\nActual output (x):\n", np.array(label[0]))
+    print("\nPredicted output (y):\n", np.array(prediction[1]), "\nActual output (y):\n", np.array(label[1]))
+    print("\nPredicted output (z):\n", np.array(prediction[2]), "\nActual output (z):\n", np.array(label[2]))
 
     # calculates and prints the normalised RMSE of the model
-    accuracy = [
-        eva.calc_accuracy(label[0], prediction[0]),
-        eva.calc_accuracy(label[1], prediction[1]),
-        eva.calc_accuracy(label[2], prediction[2])
-    ]
+    accuracy = []
+    for i in range(len(label)):
+        accuracy.append(eva.calc_accuracy(label[i], prediction[i]))
     print("\nAccuracy (x): " + str(100 * (1 - accuracy[0])) + "%")
     print("Accuracy (y): " + str(100 * (1 - accuracy[1])) + "%")
     print("Accuracy (z): " + str(100 * (1 - accuracy[2])) + "%\n")
 
     # gets the tremor component by subtracting from the voluntary motion
-    actual_tremor = [
-        np.subtract(motion[0], label[0]),  # X
-        np.subtract(motion[1], label[1]),  # Y
-        np.subtract(motion[2], label[2])  # Z
-    ]
-    predicted_tremor = [
-        np.subtract(motion[0], prediction[0]),  # X
-        np.subtract(motion[1], prediction[1]),  # Y
-        np.subtract(motion[2], prediction[2])  # Z
-    ]
+    actual_tremor = []
+    predicted_tremor = []
+    tremor_accuracy = []
+    for i in range(len(motion)):
+        actual_tremor.append(np.subtract(motion[i], label[i]))
+        predicted_tremor.append(np.subtract(motion[i], prediction[i]))
+        # calculates the normalised RMSE of the tremor component
+        tremor_accuracy.append(eva.calc_accuracy(actual_tremor[i], predicted_tremor[i]))
     tremor_error = np.subtract(actual_tremor, predicted_tremor)
-    # calculates and prints the normalised RMSE percentage of the tremor component
-    tremor_accuracy = [
-        eva.calc_accuracy(actual_tremor[0], predicted_tremor[0]),  # X
-        eva.calc_accuracy(actual_tremor[1], predicted_tremor[1]),  # Y
-        eva.calc_accuracy(actual_tremor[2], predicted_tremor[2])  # Z
-    ]
+    # converts and prints a the NRMSE in a percentage form
     print("Tremor accuracy (x): " + str(100 * (1 - tremor_accuracy[0])) + "%")
     print("Tremor accuracy (y): " + str(100 * (1 - tremor_accuracy[1])) + "%")
     print("Tremor accuracy (z): " + str(100 * (1 - tremor_accuracy[2])) + "%")
@@ -175,7 +150,7 @@ def main():
         ]
     ]
     # time is shortened to match the length of the test data
-    time = time[int(0.8 * len(data[0])):]
+    time = time[int(training_testing_ratio * len(data[0])):]
     # plots SVR model
     plot_model(time, motion[0], label[0], prediction[0], "X motion (mm)")  # x axis
     plot_model(time, motion[1], label[1], prediction[1], "Y motion (mm)")  # y axis
@@ -189,19 +164,19 @@ def main():
 
 
 def prepare_model(time, motion, labels, horizon=None):
-    # calculates the rate of change of 3D motion
-    velocity = [  # feature 2
-        fh.normalise(fh.calc_delta(time, motion[0])),  # X
-        fh.normalise(fh.calc_delta(time, motion[1])),  # Y
-        fh.normalise(fh.calc_delta(time, motion[2]))  # Z
-    ]
-
-    # calculates the rate of change of rate of change of 3D motion (rate of change of velocity)
-    acceleration = [
-        fh.normalise(fh.calc_delta(time, velocity[0])),  # X
-        fh.normalise(fh.calc_delta(time, velocity[1])),  # Y
-        fh.normalise(fh.calc_delta(time, velocity[2]))  # Z
-    ]
+    velocity = []  # feature 2
+    acceleration = []  # feature 3
+    past_motion = []  # feature 4
+    for i in range(len(motion)):
+        # calculates the rate of change of 3D motion
+        velocity.append(fh.normalise(fh.calc_delta(time, motion[i])))
+        # calculates the rate of change of rate of change of 3D motion (rate of change of velocity)
+        acceleration.append(fh.normalise(fh.calc_delta(time, velocity[i])))
+        # smoothing the velocity and acceleration
+        velocity[i] = fh.normalise(fh.calc_average(velocity[i], 5))
+        acceleration[i] = fh.normalise(fh.calc_average(acceleration[i], 5))
+        # uses the past data as a feature
+        past_motion.append(fh.normalise(fh.shift(motion[i])))
 
     # automates picking the best value for smoothing the velocity and acceleration
     # for i in range(len(velocity)):
@@ -211,68 +186,46 @@ def prepare_model(time, motion, labels, horizon=None):
     #     velocity[i] = fh.normalise(fh.calc_average(velocity[i], vel_horizon))
     #     acceleration[i] = fh.normalise(fh.calc_average(acceleration[i], accel_horizon))
 
-    # smoothing the velocity and acceleration
-    for i in range(len(velocity)):
-        velocity[i] = fh.normalise(fh.calc_average(velocity[i], 5))
-        acceleration[i] = fh.normalise(fh.calc_average(acceleration[i], 5))
-
-    # uses the past data as a feature
-    past_motion = [  # feature 3
-        fh.normalise(fh.shift(motion[0])),  # X
-        fh.normalise(fh.shift(motion[1])),  # Y
-        fh.normalise(fh.shift(motion[2]))  # Z
-    ]
-
     # finds the optimum C and horizon values if no horizon values are inputted
     if horizon is None:
-        features = [
-            [motion[0], velocity[0], past_motion[0], acceleration[0]],
-            [motion[1], velocity[1], past_motion[1], acceleration[1]],
-            [motion[2], velocity[2], past_motion[2], acceleration[2]]
-        ]
+        features = []
+        # puts all existing features in a list for model optimisation
+        for i in range(len(motion)):
+            features.append([motion[i], velocity[i], past_motion[i], acceleration[i]])
 
         # generates optimal horizon and C values
         [horizon, C] = optimise_model(features, labels)
 
-        # calculates the average 3D motion
-        average = [  # feature 4
-            fh.normalise(fh.calc_average(motion[0], horizon[0])),
-            fh.normalise(fh.calc_average(motion[1], horizon[1])),
-            fh.normalise(fh.calc_average(motion[2], horizon[2]))
-        ]
-        # adds the average feature to the features list
-        for i in range(len(features)):
-            features[i].append(average[i])
+        for i in range(len(motion)):
+            # calculates the average 3D motion
+            average = fh.normalise(fh.calc_average(motion[i], horizon[i]))  # feature 5
+            # adds the average feature to the features list
+            features[i].append(average)
         return features, horizon, C
     else:
-        # calculates the average 3D motion
-        average = [  # feature 4
-            fh.normalise(fh.calc_average(motion[0], horizon[0])),
-            fh.normalise(fh.calc_average(motion[1], horizon[1])),
-            fh.normalise(fh.calc_average(motion[2], horizon[2]))
-        ]
+        features = []
+        # puts existing features in a list
+        for i in range(len(motion)):
+            features.append([motion[i], velocity[i], past_motion[i], acceleration[i]])
 
-        return [
-            [motion[0], velocity[0], past_motion[0], acceleration[0], average[0]],
-            [motion[1], velocity[1], past_motion[1], acceleration[1], average[1]],
-            [motion[2], velocity[2], past_motion[2], acceleration[2], average[2]]
-        ]
+        for i in range(len(motion)):
+            # calculates the average 3D motion
+            average = fh.normalise(fh.calc_average(motion[i], horizon[i]))  # feature 5
+            # adds the average feature to the features list
+            features[i].append(average)
+        return features
 
 
 def optimise_model(features, labels):
     # finds the optimum value for C (regularisation parameter)
-    # print("Optimising models...")
-    # C = [  # only required to run once
-    #     op.optimise_c(features[0], labels[0]),  # X
-    #     op.optimise_c(features[1], labels[1]),  # Y
-    #     op.optimise_c(features[2], labels[2])  # Z
-    # ]
-    # horizon = [  # only required to run once
-    #     op.optimise_parameter(features[0], labels[0], "horizon"),  # X
-    #     op.optimise_parameter(features[1], labels[1], "horizon"),  # Y
-    #     op.optimise_parameter(features[2], labels[2], "horizon")  # Z
-    # ]
-    # print("Done!")
+    print("Optimising models...")
+    C = []
+    horizon = []
+    # only required to run once
+    for i in range(len(features)):
+        C.append(op.optimise_c(features[i], labels[i]))
+        horizon.append(op.optimise_parameter(features[i], labels[i], "horizon"))
+    print("Done!")
 
     # used to save time (optimising is only required once)
     C = [0.81, 0.81, 0.81]  # X, Y, Z
